cf597eda59ea2de71b2b548266a0c82aa400ab61
Switch24-7 <stephenjusto24@gmail.com>
2021-09-07 14:37:22 -0700

    Website skeleton

---
+++ application/client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CSC648 Team 3</title>
+      </Head>
+      <h1>Welcome to our website!</h1>
+    </div>
+  )
+}
