1f34a5c164c593fea4e1ed24fc8bed0c08ea74b9
Simon <swu21@mail.sfsu.edu>
2021-09-14 11:30:25 -0700

    Finished simple About me page.

--- application/client/pages/about/simon.jsx
+++ application/client/pages/about/simon.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../../styles/About.module.css'
+import styles from '../../styles/About.simon.module.css'
 
 export default function Simon(){
     return(
@@ -9,16 +9,23 @@ export default function Simon(){
             <Head>
                 <title>About Us - Simon</title>
             </Head>
-            <div className={styles.header}>
-                <h1>Simon Wu</h1>
-            </div>
-            <div>
-                <h2>Github Master</h2>
-                <p>I am a senior at SFSU majoring in Computer Science.</p>
-                <div className={styles.photoContainer}>
-                        <Image src="/about/simon/thisisfine.jpg" width={250} height={250} priority/>
+            
+               
+                <div className={styles.header}>
+                    <h1>Simon Wu</h1>
+                    <h2>Github Master</h2>
                 </div>
-            </div>
+                
+                <div className = {styles.cropped}>
+                    <div className={styles.photoContainer}>
+                        <Image src="/about/simon/thisisfine.jpg" width={250} height={250} priority/>
+                    </div>
+                </div>  
+                <p>
+                 I am a senior at SFSU majoring in Computer Science.
+                </p>
+                <h2><Link href="mailto:swu21@mail.sfsu.edu">Email</Link></h2>
+            
         </>
     )
 }
