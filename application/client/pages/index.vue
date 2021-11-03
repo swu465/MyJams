0c8d8dfffab24ac9e4e0a3ae41f8c4c28ab30a55
Cameron Paczek <cpazek@gmail.com>
2021-11-02 19:35:47 -0700

    test

--- application/client/pages/index.vue
+++ application/client/pages/index.vue
@@ -11,12 +11,14 @@
 <script>
 import axios from 'axios'
 export default {
+
   computed: {
     getLoginUrl () {
       return this.$config.apiURL + '/oauth/spotify'
     }
   },
   mounted () {
+    let x = "test";
     axios.get(this.$config.apiURL + '/me').then((response) => {
       console.log(response)
     })
