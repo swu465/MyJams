c5f2e11bc13087b7f39e04feef0c99022ef31384
Nathaniel Ray Duya <51803287+NRDuya@users.noreply.github.com>
2021-12-13 13:10:17 -0800

    Format

--- application/server/utils/addPreferences.js
+++ application/server/utils/addPreferences.js
@@ -9,7 +9,7 @@ module.exports = async function setPreferences(id, preference) {
         target_popularity: preference.target_popularity,
         target_acousticness: preference.target_acousticness
     }).then((res) => {
-        return User.updateOne({ spotifyId: res.spotifyId }, { preferenceId: res._id })
+        return User.updateOne({ spotifyId: res.spotifyId }, { preferenceId: res._id });
     }).catch((err) => {
         console.log('Error occured in setPreferences() when creating a new preference');
         console.log(err);
